99b3603ec95128efa40f91dbc3a31d359e411811
JasemDrogon <jasem.alnafisi@gmail.com>
2018-01-14 14:57:14 +0300

    Foodie after quiz 4

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -4,7 +4,10 @@ from django.urls import path, include
 from restaurants import views
 
 urlpatterns = [
-    path('restaurant_list/', views.restaurants_list.url, name='restaurant_list'),
-    path('restaurant_detail/<int:restaurant_id>/', views.restaurants_detail.url, name+'restaurant_detail')
-]
+    path('restaurant_list/', views.restaurant_list, name='restaurant_list'),
+    path('restaurant_detail/<int:restaurant_id>/', views.restaurant_detail, name='restaurant_detail'),
+    path('restaurant_update/<int:restaurant_id>/',views.restaurant_update, name="restaurant_update"),
+    path('restaurant_delete/<int:restaurant_id>/',views.restaurant_delete, name="restaurant_delete"),
+    path('restaurant_create/',views.restaurant_create, name="restaurant_create"),
 
+    ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,16 +1,64 @@
-from django.shortcuts import render
-from django.shortcuts import render
-import random
+from django.shortcuts import render, redirect
 from .models import Restaurant
+from .forms import RestaurantForm
 
 # Create your views here.
 
 def restaurant_list(request):
 	rest_objects = Restaurant.objects.all()
+	context = {
+	"restauarants": rest_objects
+	}
 
-	return render(request, 'restaurant_list.html', context)
+	return render(request, 'restaurants_list.html', context)
 
 def restaurant_detail(request, restaurant_id):
 	thing = Restaurant.objects.get(id=restaurant_id)
+	context = {
+		"festaurant": thing
+	}
+	return render(request, 'restaurants_detail.html', context)
 
-	return render(request, 'restaurant_detail.html', context)
+
+
+def restaurant_create(request):
+	form =RestaurantForm(request.POST or None)
+	if form.is_valid():
+
+		form.save()
+
+		return redirect("restaurant_list")
+	context = {
+
+	"restaurant" : form 
+	}
+
+	return render(request, "restaurant_create.html", context)
+
+def restaurant_update(request,restaurant_id):
+
+	item = Restaurant.objects.get(id= restaurant_id)
+	form = RestaurantForm(instance = item)
+
+	if request.method == "POST":
+
+		form = RestaurantForm(request.POST or None, instance = item)
+
+	
+	if form.is_valid():
+		form.save()
+		return redirect("restaurant_detail", restaurant_id=restaurant_id)
+
+	context = {
+
+	"form": form,
+	"item": item,	
+
+	}
+
+	return render(request, "restaurant_update.html", context)
+
+def restaurant_delete(request,restaurant_id):
+	Restaurant.objects.get(id=restaurant_id).delete()
+
+	return redirect("restaurant_list")
